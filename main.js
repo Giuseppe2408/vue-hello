c83191bc0afc93f3b6307d1a96f0e533b23ff7c3
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-24 16:39:02 +0200

    fatto

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = new Vue({
+    el: '#container',
+    data: {
+        message: "Hello World",
+        image: "https://picsum.photos/200",
+        alt : "immagine random",
+    }
+});
